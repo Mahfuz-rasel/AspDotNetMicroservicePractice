7a76895c01e86040efd2d0b6be6c9c197dcfa440
mahfuzphycu <mahfuz.phycu@gmail.com>
2022-05-23 00:56:04 +0600

    Add Ordering API

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Application.Modals;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+   public interface IEmailServices
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
